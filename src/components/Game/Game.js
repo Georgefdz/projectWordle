a2bac8535e9d7f1b1532c85ada038cb28aea9db8
George Fernandez <georgfdz@gmail.com>
2024-12-07 14:09:08 -0600

    Feat: Excercise 1

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,36 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [input, setInput] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({ guess: input });
+    setInput("");
+  }
+
+  return (
+    <>
+      <form
+        className="guess-input-wrapper"
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          value={input}
+          onChange={(event) => {
+            const newInput = event.target.value.toUpperCase();
+            setInput(newInput);
+          }}
+          title="5 letter word"
+          pattern="[a-zA-Z]{5}"
+          // placeholder="Write a 5 letter word"
+        />
+      </form>
+    </>
+  );
 }
 
 export default Game;
